262b46fb0d8c8f6f62ed14e678e21649d9322fd5
Daniel Shi(施建合) <Daniel_shi@asus.com>
2021-12-10 10:29:16 +0800

    Lesson1 基本vue架構，看main.js註解跟App.vue註解就有了

---
+++ main/src/App.vue
@@ -0,0 +1,36 @@
+<!-- 他的定義很嚴謹，如果我templete裡面沒有用到HelloWorld，但component裡面有放，表示放了沒有到，會報錯，如果import了沒有放到component，也會報錯 -->
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>To-Do List</h1>
+      <ul>
+        <li>
+          <todoItem />
+          <todoItem />
+        </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import todoItem from './components/todoItem.vue'
+
+export default {
+  name: 'App',
+  components: {
+    todoItem
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ main/src/components/todoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <input type="checkbox" id="todo-item" />
+    <label for="todo-item">My Todo Item</label>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'todoItem',
+}
+
+</script>
+
