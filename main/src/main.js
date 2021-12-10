f661655f2eaa1883782302ba0f4216aa6352bef1
lystyp <lystyp87015@gmail.com>
2021-12-10 16:12:00 +0800

    Revert "Lesson1 基本vue架構，看main.js註解跟App.vue註解就有了"
    This reverts commit 262b46fb0d8c8f6f62ed14e678e21649d9322fd5.

--- main/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
-// App裡面包含"樣版"跟"export出來的物件"，丟進h裡面渲染，應該就是先讓index.html import App.vue的script區塊，再把App.vue裡面樣版塞到index.html的#app裡面
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
